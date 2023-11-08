b26851bece2d559d929cb68108ef1c57b8579c17
alifhasanshah <alifhasanshah@gmail.com>
2023-11-09 02:45:55 +0600

    assignment create api done

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
+const cookieParser = require('cookie-parser')
+
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173'],
+  credentials: true
+}));
 app.use(express.json());
+app.use(cookieParser())
 
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@ph-8.7tjeuwe.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -24,7 +30,17 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    // database and collection
+    const database = client.db('assignmentsDB');
+    const assignmentsCollection = database.collection('assignments');
 
+    // assignment api
+    app.post('/api/v1/create-assignment', async(req, res) => {
+      const assignment = req.body;
+      console.log(assignment);
+      const result = await assignmentsCollection.insertOne(assignment);
+      res.send(result);
+    })
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
