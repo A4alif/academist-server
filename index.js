37de9cafc829c76bbfa892ba3b47f68af2cb4e20
alifhasanshah <alifhasanshah@gmail.com>
2023-11-09 14:28:42 +0600

    vercel client link added to cors

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors({
-  origin: ['http://localhost:5173'],
+  // origin: ['http://localhost:5173'],
+  origin: ['https://assignment-client-ten.vercel.app'],
   credentials: true
 }));
 app.use(express.json());
